cc0051a4b3419787db7801f432e6255d0d49863e
AJ Fagan <ajfagan@wisc.edu>
2024-09-18 19:54:06 -0500

    temporarily removing login manager

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 { home-manager, nixpkgs, ...}:
 let
 in {
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.xserver.enable = true;
+  #services.displayManager.sddm.enable = true;
+  #services.displayManager.sddm.wayland.enable = true;
+  #services.desktopManager.plasma6.enable = true;
   services.getty.autologinUser = "aj";
 
   system.stateVersion = "24.11";
